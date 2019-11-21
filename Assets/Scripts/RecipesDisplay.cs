aa69cd1724fed849b792adfbcd07856c09166685
raphaellndr <54632013+raphaellndr@users.noreply.github.com>
2019-11-21 16:26:34 +0100

    Menus

---
+++ Assets/Scripts/RecipesDisplay.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipesDisplay : MonoBehaviour
+{
+    int speedTime = 50;
+
+    int i = 0;
+
+    public Sprite firstImage;
+
+    Image myImageComponent;
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("RecipeNumber", 0);
+
+        InvokeRepeating("Create", 5f, 5f);
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("RecipeNumber", 0) != 0)
+        {
+            GameObject myObject = GameObject.Find(PlayerPrefs.GetInt("RecipeNumber", 0).ToString());
+
+            //Move the Image
+            myObject.transform.Translate(Vector2.up * Time.deltaTime * speedTime );
+        }
+
+        if (PlayerPrefs.GetInt("RecipeNumber", 0) >= 2)
+        {
+            CancelInvoke();
+        }
+    }
+    void Create()
+    {
+        i++;
+        PlayerPrefs.SetInt("RecipeNumber", i);
+
+        //Create Canvas
+        GameObject canvas = CreateCanvas(false);
+
+        //Create your Image GameObject
+        GameObject newObject = new GameObject(i.ToString());
+
+        //Add a Tag to the Image GameObject
+        //newObject.gameObject.tag = "Recipe" + i.ToString();
+
+        //Make the GameObject child of the Canvas
+        newObject.transform.SetParent(canvas.transform);
+
+        //Add Image Component to it(This will add RectTransform as-well)
+        newObject.AddComponent<Image>();
+
+        //Add a sprite to the Image
+        myImageComponent = newObject.GetComponent<Image>();
+        myImageComponent.sprite = firstImage;
+
+        //Set the position of the Image
+        newObject.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        newObject.transform.position = new Vector2(50f, 150f);
+    }
+
+    //Creates Hidden GameObject and attaches Canvas component to it
+    private GameObject CreateCanvas(bool hide)
+    {
+        //Create Canvas GameObject
+        GameObject tempCanvas = new GameObject("Canvas");
+        if (hide)
+        {
+            tempCanvas.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        //Create and Add Canvas Component
+        Canvas cnvs = tempCanvas.AddComponent<Canvas>();
+        cnvs.renderMode = RenderMode.ScreenSpaceOverlay;
+        cnvs.pixelPerfect = false;
+
+        //Set Cavas sorting order to be above other Canvas sorting order
+        cnvs.sortingOrder = 12;
+
+        cnvs.targetDisplay = 0;
+
+        AddCanvasScaler(tempCanvas);
+        return tempCanvas;
+    }
+
+    //Adds CanvasScaler component to the Canvas GameObject 
+    private void AddCanvasScaler(GameObject parentaCanvas)
+    {
+        CanvasScaler cvsl = parentaCanvas.AddComponent<CanvasScaler>();
+        cvsl.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        cvsl.referenceResolution = new Vector2(800f, 600f);
+        cvsl.matchWidthOrHeight = 0.5f;
+        cvsl.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+        cvsl.referencePixelsPerUnit = 100f;
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public ScoreManager[] scores;
+
+    public Text score1;
+    public Text score2;
+    public Text highScore1;
+    public Text highScore2;
+
+    public int number1 = 0;
+    public int number2 = 0;
+
+    void Start()
+    {
+        highScore1.text = PlayerPrefs.GetInt("HighScore1", 0).ToString();
+        //highScore2.text = PlayerPrefs.GetInt("HighScore2", 0).ToString();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            number1 += 1;
+
+            score1.text = number1.ToString();
+
+            if (number1 > PlayerPrefs.GetInt("HighScore1",0))
+            {
+                PlayerPrefs.SetInt("HighScore1", number1);
+                highScore1.text = number1.ToString();
+            } 
+        }
+
+/*        if (Input.GetKeyDown(KeyCode.S))
+        {
+            number2 += 1;
+            score2.text = number2.ToString();
+
+            if (number2 > PlayerPrefs.GetInt("HighScore2", 0))
+            {
+                PlayerPrefs.SetInt("HighScore2", number2);
+                highScore2.text = number2.ToString();
+            }
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();            //or DeleteKey("");
+        highScore1.text = "0";
+        //highScore2.text = "0";
+    }
+}
