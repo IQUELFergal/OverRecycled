9ab1957e23335ec60f1d39196703ac0c151c35c4
raphaellndr <54632013+raphaellndr@users.noreply.github.com>
2019-11-22 18:12:36 +0100

    Use of the gravity to move the recipe

--- Assets/Scripts/RecipeDisplayer.cs
+++ Assets/Scripts/RecipeDisplayer.cs
@@ -3,41 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RecipesDisplay : MonoBehaviour
+public class RecipeDisplayer : MonoBehaviour
 {
-    int speedTime = 50;
-
     int i = 0;
 
     public Sprite firstImage;
 
     Image myImageComponent;
 
+    Rigidbody2D constraint;
+    BoxCollider2D colliderScaler;
+
     void Start()
     {
         PlayerPrefs.SetInt("RecipeNumber", 0);
 
-        InvokeRepeating("Create", 5f, 5f);
+        InvokeRepeating("Create", 0f, 5f);
+
+        Physics2D.gravity = new Vector2(0, 200f);  
     } 
 
     // Update is called once per frame
     void Update()
     {
-        if (PlayerPrefs.GetInt("RecipeNumber", 0) != 0)
-        {
-            GameObject myObject = GameObject.Find(PlayerPrefs.GetInt("RecipeNumber", 0).ToString());
-
-            //Move the Image
-            myObject.transform.Translate(Vector2.up * Time.deltaTime * speedTime );
-        }
-
-        if (PlayerPrefs.GetInt("RecipeNumber", 0) >= 2)
+        if (PlayerPrefs.GetInt("RecipeNumber", 0) >= 3)
         {
             CancelInvoke();
         }
+        if (PlayerPrefs.GetInt("RecipeNumber", 0) != 0)
+        {
+            
+
+        }
     }
+
     void Create()
     {
+        //Number of recipe
         i++;
         PlayerPrefs.SetInt("RecipeNumber", i);
 
@@ -47,8 +49,15 @@ public class RecipesDisplay : MonoBehaviour
         //Create your Image GameObject
         GameObject newObject = new GameObject(i.ToString());
 
-        //Add a Tag to the Image GameObject
-        //newObject.gameObject.tag = "Recipe" + i.ToString();
+        //Add a Rigidbody2D to the Image Gameobject and freeze the rotation
+        newObject.AddComponent<Rigidbody2D>();
+        constraint = newObject.GetComponent<Rigidbody2D>();
+        constraint.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        //Add a BoxCollider2D to the Image Gameobject and scale it
+        newObject.AddComponent<BoxCollider2D>();
+        colliderScaler = newObject.GetComponent<BoxCollider2D>();
+        colliderScaler.size = new Vector2(100f, 100f);
 
         //Make the GameObject child of the Canvas
         newObject.transform.SetParent(canvas.transform);
@@ -61,8 +70,9 @@ public class RecipesDisplay : MonoBehaviour
         myImageComponent.sprite = firstImage;
 
         //Set the position of the Image
-        newObject.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        //newObject.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
         newObject.transform.position = new Vector2(50f, 150f);
+        newObject.transform.localScale = new Vector2(1.5f, 1.5f);
     }
 
     //Creates Hidden GameObject and attaches Canvas component to it
@@ -94,7 +104,7 @@ public class RecipesDisplay : MonoBehaviour
     {
         CanvasScaler cvsl = parentaCanvas.AddComponent<CanvasScaler>();
         cvsl.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-        cvsl.referenceResolution = new Vector2(800f, 600f);
+        cvsl.referenceResolution = new Vector2(1920f, 1080f);
         cvsl.matchWidthOrHeight = 0.5f;
         cvsl.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
         cvsl.referencePixelsPerUnit = 100f;
