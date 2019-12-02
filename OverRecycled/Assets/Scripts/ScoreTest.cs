72d852b861e00ec86c723c99c39b0e7bfe456ea8
IQUELFergal <fergal.iquel@gmail.com>
2019-12-02 15:10:52 +0100

    Fix conflicting class name

--- OverRecycled/Assets/Scripts/ScoreTest.cs
+++ OverRecycled/Assets/Scripts/ScoreTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Score : MonoBehaviour
+public class ScoreTest : MonoBehaviour
 {
     public ScoreManager[] scores;
 
