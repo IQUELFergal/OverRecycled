e0971fed59d89ba48b8692c852c0ac7156da8cea
IQUELFergal <fergal.iquel@gmail.com>
2019-12-05 15:23:33 +0100

    Menu rework

--- OverRecycled/Assets/Scripts/ScoreTest.cs
+++ OverRecycled/Assets/Scripts/ScoreTest.cs
@@ -5,23 +5,15 @@ using UnityEngine.UI;
 
 public class ScoreTest : MonoBehaviour
 {
-    public ScoreManager[] scores;
-
     public Text score1;
-    public Text score2;
     public Text highScore1;
-    public Text highScore2;
 
     int number1 = 0;
-    //int number2 = 0;
 
     void Start()
     {
         highScore1.text = PlayerPrefs.GetInt("HighScore1", 0).ToString();
-        //highScore2.text = PlayerPrefs.GetInt("HighScore2", 0).ToString();
-
         score1.text = number1.ToString();
-
         PlayerPrefs.SetInt("CurrentScore1", number1);
     }
 
@@ -41,18 +33,6 @@ public class ScoreTest : MonoBehaviour
                 highScore1.text = number1.ToString();
             } 
         }
-
-/*        if (Input.GetKeyDown(KeyCode.S))
-        {
-            number2 += 1;
-            score2.text = number2.ToString();
-
-            if (number2 > PlayerPrefs.GetInt("HighScore2", 0))
-            {
-                PlayerPrefs.SetInt("HighScore2", number2);
-                highScore2.text = number2.ToString();
-            }
-        }*/
 
         if (Input.GetKeyDown(KeyCode.R))
         {
--- OverRecycled/Assets/Scripts/Timer.cs
+++ OverRecycled/Assets/Scripts/Timer.cs
@@ -9,6 +9,13 @@ public class Timer : MonoBehaviour
     public float timeLeft = 60.0f;
     public Text timerText;
     private bool isGameOver;
+    public Canvas gameOverUI;
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+        gameOverUI.gameObject.SetActive(false);
+    }
 
     void Update()
     {
@@ -23,7 +30,7 @@ public class Timer : MonoBehaviour
                 timerText.text = actualResult.ToString("ss");
                 if (timeLeft <= 10f)
                 {
-                    timerText.transform.localScale = new Vector3(1+Mathf.PingPong(Time.time,0.5f), 1 + Mathf.PingPong(Time.time, 0.5f), 0);
+                    timerText.transform.localScale = new Vector3(0.5f+Mathf.PingPong(Time.time/4,0.125f), 0.5f + Mathf.PingPong(Time.time/4,0.125f), 0);
                     timerText.color = new Color(1, Mathf.PingPong(2 * Time.time, 1), Mathf.PingPong(2 * Time.time, 1));
                     
                 }
@@ -46,6 +53,11 @@ public class Timer : MonoBehaviour
     void GameOver() //Script executé à la fin du timer
     {
         Debug.Log("Game Over");
+        Time.timeScale = 0f;
+        gameOverUI.gameObject.SetActive(true);
     }
 
 }
+
+
+
